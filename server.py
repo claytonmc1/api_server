95e827fab0fa191a79d5e87a88ba0784f61d02e6
Clayton Marchi <claytonmc1@gmail.com>
2020-03-09 16:05:16 -0300

    Correção

--- server.py
+++ server.py
@@ -2,6 +2,7 @@ from flask import Flask, request, jsonify
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 from json import dumps
+##
 
 db_connect = create_engine('sqlite:///exemplo.db')
 app = Flask(__name__)
